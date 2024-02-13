9f33826718fec5f48a6ec3d4058ff931d7166a3c
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2024-02-13 13:59:49 +0700

    add score table

--- src/components/update/Visualize/ShapeletVisualize.tsx
+++ src/components/update/Visualize/ShapeletVisualize.tsx
@@ -1,15 +1,19 @@
-import { memo, useContext, useEffect } from "react"
 import Plot from "react-plotly.js";
+import 'react-tabulator/lib/styles.css';
+import { ReactTabulator } from 'react-tabulator'
 
 const ShapeletVisualize = (nodeVisualize) => {
-    const visualize = nodeVisualize.nodeVisualize
+    const visualize = nodeVisualize.nodeVisualize["shapelet_transformation"]
 
     var data = new Array()
 
     const shapelets = visualize["shapelets"]
     const scores = visualize["scores"]
     const labels = visualize["labels"]
+    const criterion = visualize["criterion"]
 
+    // graph
+    const uniqueLabels = [...new Set(labels)]
     shapelets.forEach((sl, idx) => {
         // console.log("this is len", sl.length)
         data.push({
@@ -20,17 +24,46 @@ const ShapeletVisualize = (nodeVisualize) => {
             name: `Shapelet ${idx} (label: ${labels[idx]})`
         })
     })
-
     var layout = {
         title: 'Shapelets',
     }
 
+    // score table
+    var columns = [
+        { title: "Name", field: "name" },
+        { title: "Score", field: "score"},
+    ]
+    var tableData = []
+    scores.forEach((score, idx) => {
+        tableData.push({
+            id: idx,
+            name: `Shapelet ${idx}`,
+            score: score,
+        })
+    });
+
     return (
         <div>
             <h1>Discovered Shapelets</h1>
+
+            <div>
+            <label htmlFor="shapeletIndex">Label: </label>
+            <select name="shapeletIndex" id="labelFilter">
+                <option value="all">all</option>
+                {uniqueLabels.map((v: string) => {
+                    return (<option value={String(v)}>{v}</option>)
+                })}
+            </select>
+            </div>
+
             <Plot
                 data={data}
                 layout={layout}
+            />
+
+            <ReactTabulator
+                data={tableData}
+                columns={columns}
             />
         </div>
     )
