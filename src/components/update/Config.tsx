52f70c2f79ee1bd02fb0407c38961bf8dadad085
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2024-03-31 18:13:48 +0700

    update prep node config

--- src/components/update/Config.tsx
+++ src/components/update/Config.tsx
@@ -11,6 +11,10 @@ export const Config: React.FC<ConfigProps> = ({ data }) => {
 	let [disableVisualizeButton, setDisableVisualizeButton] = useState(true)
 	const [nodeConfig, setNodeConfig] = useState<any[]>([]);
 	const [updateData, setUpdateData] = useState<any>();
+
+	// newly added for updated config panels
+	const [prepInstructions, setPrepInstructions] = useState<any[]>([]);
+
   const prepOptions = [
     { label: 'TestPrep', value: 'TestPrep' },
     { label: 'TrainPrep', value: 'TrainPrep' }
@@ -148,32 +152,149 @@ export const Config: React.FC<ConfigProps> = ({ data }) => {
 	}
 
 	function renderKwargsPrepNode() {
+		const TAG = "PREP DEBUG"
+
+		// read previous instructions
+		var instructions = updateData?.kwargs?.instructions || []
+		var setRoleInstructions = []
+		var changeTypeInstructions = []
+		instructions.forEach((instructions, idx) => {
+			if (instructions[0] == "set_role") {
+				setRoleInstructions.push(instructions)
+			} else {
+				changeTypeInstructions.push(instructions)
+			}
+		})
+
+		console.log(TAG, "instructions lists", setRoleInstructions, changeTypeInstructions)
+
+		function onAddSetRole(e) {
+			var newInstructions = ["set_role", "", ""]
+			setRoleInstructions.push(newInstructions)
+			updateInstructions()
+		}
+
+		function onSetRoleChange(e) {
+			var idx = parseInt(e.target.parentNode.id.split("-")[3])
+			setRoleInstructions[idx][1] = e.target.value
+			updateInstructions()
+		}
+
+		function onRemoveSetRole(e) {
+			var idx = parseInt(e.target.parentNode.id.split("-")[3])
+			setRoleInstructions.splice(idx, 1) // remove one elements
+			updateInstructions()
+		}
+
+		function onAddChangeType(e) {
+			var newInstructions = ["change_type", "", "int"]
+			changeTypeInstructions.push(newInstructions)
+			updateInstructions()
+		}
+
+		function onChangeTypeChange(e) {
+			var idx = parseInt(e.target.parentNode.id.split("-")[3])
+			changeTypeInstructions[idx][1] = e.target.value
+			updateInstructions()
+		}
+
+		function onRemoveChangeType(e) {
+			var idx = parseInt(e.target.parentNode.id.split("-")[3])
+			changeTypeInstructions.splice(idx, 1) // remove one elements
+			updateInstructions()
+		}
+
+		function updateInstructions() {
+			instructions = setRoleInstructions.concat(changeTypeInstructions)
+			setPrepInstructions(instructions)
+			setUpdateData(prevData => ({...prevData, kwargs: { ...prevData.kwargs, instructions: instructions}}))
+			console.log(instructions)
+		}
+
 		return(
 		<div className='kwagrs-style'> 
 			<h3>Prep Node</h3>
-			<select 
-				value={updateData?.name} 
-				onChange={(e) => {
-					const newValue = e.target.value;
-					setUpdateData(prevData => ({
-							...prevData,
-							name: newValue
-					}));
-				}}>
-				<option value="">Select an option</option>
-				{prepOptions.map(option => (
-					<option key={option.value} value={option.value}>
-						{option.label}
-					</option>
-				))}
-			</select>
-			<div className="input-container">
+
+			<h4>Set Role</h4>
+			{ setRoleInstructions.map((instruction, idx) => {
+				var previousColumn = instruction[1]
+				return (
+					<div id={`instruction-set-role-${idx}`}>
+						<label>Column:</label>
+						<input 
+							type='text' 
+							defaultValue={previousColumn}
+							onChange={(e) => { onSetRoleChange(e) }}
+						/>
+						<label>Role:</label>
+						<select name='role'>
+							<option value="target" selected>Target</option>
+						</select>
+						<button onClick={(e) => {onRemoveSetRole(e)}}>del</button>
+					</div>
+				)
+			})}
+			<button onClick={ (e) => onAddSetRole(e) }>
+				+
+			</button>
+
+			<h4>Change Type</h4>
+			{ changeTypeInstructions.map((instruction, idx) => {
+				var previousColumn = instruction[1]
+				return (
+					<div id={`instruction-change-type-${idx}`}>
+						<label>Column:</label>
+						<input 
+							type='text' 
+							defaultValue={previousColumn}
+							onChange={(e) => { onChangeTypeChange(e) }}
+						/>
+						<label>Type:</label>
+						<select name='type'>
+							<option value="int" selected>int</option>
+						</select>
+						<button onClick={(e) => {onRemoveChangeType(e)}}>del</button>
+					</div>
+				)
+			})}
+			<button onClick={ (e) => onAddChangeType(e) }>
+				+
+			</button>
+
+			{/* { instructions.map((instruction, idx) => {
+				// TODO: rerender previous instructions
+				// TODO: add new instruction here
+				var defaultInstruction = instruction[0]
+				var defaultType = instruction[1]
+				var defaultCol = instruction[2]
+				
+				return (<div id={`instruction-${idx}`}>
+					<div>
+						<label>Instruction:</label>
+						<select 
+							name="instruction" 
+						>
+							<option value={"set_role"}>Set Role</option>
+							<option value={"change_type"} selected>Change Type</option>
+						</select>
+					</div>
+					<div>
+						<label>Column:</label>
+						<input 
+							name='feature' 
+							type='text' 
+							value={defaultType}
+						/>
+					</div>
+				</div>)
+			}) } */}
+
+			{/* <div className="input-container">
 				<label>Set Role:</label>
 				<input
 					type="text"
 					value={updateData?.kwargs?.instructions?.[0]?.[2]}
 					onChange={(e) => setUpdateData(prevData => ({ ...prevData, kwargs: { ...prevData.kwargs, instructions: [[prevData.kwargs.instructions[0][0], prevData.kwargs.instructions[0][1], e.target.value], prevData.kwargs.instructions[1]] } }))}
-			
 				/>
 			</div>
 			<div className="input-container">
@@ -183,7 +304,7 @@ export const Config: React.FC<ConfigProps> = ({ data }) => {
 					value={updateData?.kwargs?.instructions?.[1]?.[2]}
 					onChange={(e) => setUpdateData(prevData => ({ ...prevData, kwargs: { ...prevData.kwargs, instructions: [prevData.kwargs.instructions[0], [prevData.kwargs.instructions[1][0], prevData.kwargs.instructions[1][1], e.target.value]] } }))}
 				/>
-			</div>
+			</div> */}
 		</div>)
 	}
 
